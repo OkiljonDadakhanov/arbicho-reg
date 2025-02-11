7548bdaa90f33690da3686cd18416b74c54adacb
Akilhan <oqiljondadaxanov@gmail.com>
2025-02-11 15:31:34 +0500

    public api

--- components/register.tsx
+++ components/register.tsx
@@ -27,7 +27,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+const baseUrl = "https://api.olympcenter.uz/";
 
 const formSchema = z.object({
   full_name: z
